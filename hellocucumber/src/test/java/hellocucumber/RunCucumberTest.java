bf483088cedd1ecfd1918db9687f2c1748f04736
IUT BLAGNAC <noreply@univ-tlse2.fr>
2023-12-15 15:43:15 +0100

    Une avancée dans le TP4...

--- hellocucumber/src/test/java/hellocucumber/RunCucumberTest.java
+++ hellocucumber/src/test/java/hellocucumber/RunCucumberTest.java
@@ -4,12 +4,20 @@ import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("hellocucumber")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber.json")
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/hellocucumber",
+    glue = "com.hellocucumber.stepdefinitions"
+)
 public class RunCucumberTest {
 }
